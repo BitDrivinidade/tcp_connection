3073e417890dbc2dfe0ba9942821af22f623f0c2
USUARIO <celio8625@gmail.com>
2025-08-16 03:01:48 +0100

    ultimo

--- socket_aula.py
+++ socket_aula.py
@@ -3,8 +3,8 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.settimeout(1)
 try:
-    client.connect(('192.168.1.24', 8080))
-    client.send(b'Ola Mundo\n\n')
+    client.connect(('host_adress(255.255.255.255)', 8080))
+    client.send(b'Hello World\n\n')
     response = client.recv(1024).decode()
     print(response)
 except Exception as e:
